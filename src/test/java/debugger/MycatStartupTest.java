cbdcaeaef5e4d8c0c7defc95e725447637e60fba
hongbo.zhao <1005742198@qq.com>
2017-10-13 17:07:18 +0800

    芋道源码第一期内容

---
+++ src/test/java/debugger/MycatStartupTest.java
@@ -0,0 +1,14 @@
+package debugger;
+
+import io.mycat.MycatStartup;
+
+/**
+ * Created by za-zhaohongbo on 2017/10/12.
+ */
+public class MycatStartupTest {
+
+    public static void main(String[] args) {
+        MycatStartup.main(args);
+    }
+
+}
